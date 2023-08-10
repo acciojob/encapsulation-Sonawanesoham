aa667faaa4814825d872963723bfdd778cd9ca1b
Soham <sonawanesoham2002@gmail.com>
2023-08-11 00:23:30 +0530

    Example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+        /*
+        rw.name = "Test";
+        System.out.println(name);
+
+        Getting below-mentioned error after Task-4:
+            java: name has private access in com.driver.RWOnly
+        */
+
+        rw.setName("Test");
+        System.out.println(rw.getName());
+
+    }
 }
